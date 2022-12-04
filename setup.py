ecc6477928f20a2503b893cf7f2e67e6995c6c77
akashkalam <akashkalam1020>
2022-12-04 17:47:44 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-from setuptools import find_namespace_packages,setup
+from setuptools import find_packages,setup
+
 from typing import List
 
-REQUIREMENTS_FILE_NAME = 'requirements.txt'
-HYPEN_E_DOT = '-E .'
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements()->List[str]:
-    with open(REQUIREMENTS_FILE_NAME) as requirement_file:
+    
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
-    requirement_list = [requirement_name.replace("\n","")for requirement_name in requirement_list]
-    if HYPEN_E_DOT in requirement_list:
-        requirement_list.remove(HYPEN_E_DOT)
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
+
 
 
 setup(
     name="sensor",
-    version="0.0.1",
-    author='akashkalam',
-    author_email='akashkalam1020@gmail.com',
-    packages=find_packages(),
-    install_requirements=get_requirements(),
+    version="0.0.2",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
+    packages = find_packages(),
+    install_requires=get_requirements(),
 )
