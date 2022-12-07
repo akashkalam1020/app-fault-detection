5c783a5d26ea66b364dd239a3edc7ce5f805b2de
akashkalam <akashkalama1020@gmail.com>
2022-12-07 14:50:34 +0530

    Updated files

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 import sys, os
 from sensor.entity import config_entity
+from sensor.components import data_ingestion
+from sensor.components.data_ingestion import DataIngestion
 
 if __name__=="__main__":
     try:
-        TrainingPipelineConfig = config_entity.TrainingPipelineConfig()
-        data_ingestion_config = DataIngestionConfig(training_pipeline_config = training_pipeline_config)
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config = training_pipeline_config)
         print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        print(data_ingestion.initiate_data_ingestion())
     except Exception as e:
         print(e)
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -3,12 +3,17 @@ from sensor.exception import SensorException
 from sensor.logger import logging
 from datetime import datetime
 
+FILE_NAME = "sensor.csv"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
 class TrainingPipelineConfig:
+
     def __init__(self):
         try:
             self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
         except Exception  as e:
-            raise SensorException(e,sys)  
+            raise SensorException(e,sys)     
 
 class DataIngestionConfig:
 
@@ -22,15 +27,16 @@ class DataIngestionConfig:
             self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
             self.test_size = 0.2
         except Exception  as e:
-            raise SensorException(e,sys)    
-            
+            raise SensorException(e,sys)     
+
     def to_dict(self,)->dict:
         try:
             return self.__dict__
         except Exception  as e:
-            raise SensorException(e,sys)   
+            raise SensorException(e,sys)
 
 class DataValidationConfig:...
 class DataTransformationConfig:...
 class ModelTrainerConfig:...
 class ModelEvaluationConfig:...
+class ModelPusherConfig:...
